49e59cca015966fc85c9c2c0c6cc24c96a273df7
a17100262 <maueezahmed@gmail.com>
2018-04-23 14:23:44 +0000

    seed file changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-10.times do
-    Company.create!(name: Faker::Company.name)
-end
+# 10.times do
+#     Company.create!(name: Faker::Company.name)
+# end
 
 # User.create!(email:"super_admin@test.com",password: "admin123",role: "super_admin")
-User.create!(email:"admin@test.com",password: "admin123",role: "admin", company_id: Company.all.pluck(:id).sample)
+User.create!(email:"super@test.com",password: "admin123",role: "super_admin")
